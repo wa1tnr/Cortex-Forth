8b482fce0663dceed60678bdf38dd3b87c5e5161
wa1tnr <wa1tnr@users.noreply.github.com>
2019-07-29 14:48:37 +0000

    Sample session in Forth On branch exp-ee 	new file:   session-aaa.fs

---
+++ doc/forth/session-aaa.fs
@@ -0,0 +1,16 @@
+warm
+\ [press ENTER on a line by itself]
+words
+wlist
+: emits 0 do emit loop ;
+64 8 + emit
+64 8 + 32 + emit
+64 8 + 32 + 3 - emit
+64 8 + 32 + 3 - 7 + emit
+64 8 + 32 + 3 - 7 + 3 + emit
+64 8 + . 72
+64 8 + 32 + 3 - . 101
+64 8 + 32 + 3 - 7 + . 108
+64 8 + 32 + 3 - 7 + 3 + . 111
+111 108 108 101 72 5 emits
+
