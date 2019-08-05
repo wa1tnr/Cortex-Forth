c77ef27f5a5bfe8510a900e369e8f91bcd4a3fe7
wa1tnr <wa1tnr@users.noreply.github.com>
2019-08-05 23:56:34 +0000

    prep for 0.1.7 reversed-ff 	modified:   Cortex-Forth.ino On branch reversed-ff

--- Cortex-Forth/Cortex-Forth.ino
+++ Cortex-Forth/Cortex-Forth.ino
@@ -317,7 +317,7 @@ void _DDOTS (void) {
   }
   _DUP ();
   W = S0;
-  while (W > S) {
+  while (W > (S + 1)) {
     Serial.print (memory.data [--W]);
     Serial.write (' ');
   }
@@ -592,7 +592,7 @@ void setup () {
 //  I = 200; //  test
   Serial.begin (9600);
   while (!Serial);
-  Serial.println ("myForth Arm Cortex - release 0.1.6 (reversed-ee) de wa1tnr - use CTRL J and try the words word ");
+  Serial.println ("myForth Arm Cortex - release 0.1.7 (reversed-ff) de wa1tnr - use CTRL J and try the words word");
   //  _WORDS ();
   //  _DEPTH ();
   _DDOTS ();
