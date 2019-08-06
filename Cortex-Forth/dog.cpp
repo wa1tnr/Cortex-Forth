c75d8cd082491801c9f0d250fd6729ed8566c79d
wa1tnr <wa1tnr@users.noreply.github.com>
2019-08-06 01:45:50 +0000

    timestamped. 	modified:   dog.cpp On branch develop

--- Cortex-Forth/dog.cpp
+++ Cortex-Forth/dog.cpp
@@ -1,1 +1,3 @@
-// privak gisgis kualdo  - Tue Aug  6 00:16:30 UTC 2019 0.1.8 reversed-gg
+// kinten  privak gisgis kualdo  - Tue Aug  6 00:16:30 UTC 2019 0.1.8 reversed-gg
+
+// Tue Aug  6 01:43:55 UTC 2019
