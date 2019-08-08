f4a1547771d66797e7fda5de6d24d4b863880ba5
wa1tnr <wa1tnr@users.noreply.github.com>
2019-08-08 21:07:16 +0000

    timestamps and such
    	modified:   dog.cpp
    
    On branch fload-bb-aa

--- Cortex-Forth/dog.cpp
+++ Cortex-Forth/dog.cpp
@@ -1,3 +1,25 @@
-// kinten  privak gisgis kualdo  - Tue Aug  6 00:16:30 UTC 2019 0.1.8 reversed-gg
+// kinten  privak gisgis kualdo  - Thu Aug  8 20:25:10 UTC 2019 0.1.8 fload-bb-aa
+// steyva  fanipi  telco robit
 
-// Tue Aug  6 01:43:55 UTC 2019
+// Thu Aug  8 20:25:10 UTC 2019
+
+// Other branches:
+/*
+  fload-aa
+* fload-bb-aa
+  master
+  reversed-aa
+  reversed-bb
+  reversed-cc
+  reversed-dd
+  reversed-ee
+  reversed-ff
+  reversed-gg
+  srev-gg-aa
+  srev-gg-aa-ris-aa
+*/
+
+// Note that the present branch came from branch exp-m-gg in
+// Steno-Keyboard-Arduino-tnr and was chosen since it already
+// has the full vocabulary, including defining words : and ;
+// available.
