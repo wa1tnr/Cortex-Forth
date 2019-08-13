7be0c08b4bb5360b2210b215501fae995e7645ea
wa1tnr <wa1tnr@users.noreply.github.com>
2019-08-13 00:02:00 +0000

    flash operations
    	new file:   flash_ops.cpp
    
    On branch good-compiler-aa-bb

---
+++ Cortex-Forth/src/flash_ops.cpp
@@ -0,0 +1,135 @@
+/*
+  SD card read/write
+
+ This example shows how to read and write data to and from an SD card file
+ The circuit:
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+
+ created   Nov 2010
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ */
+
+#include <SPI.h>
+#include "SdFat.h"
+#include "Adafruit_SPIFlash.h"
+
+File thisFile;
+#include "../common.h"
+
+#if defined(__SAMD51__) || defined(NRF52840_XXAA)
+  Adafruit_FlashTransport_QSPI flashTransport(PIN_QSPI_SCK, PIN_QSPI_CS, PIN_QSPI_IO0, PIN_QSPI_IO1, PIN_QSPI_IO2, PIN_QSPI_IO3);
+#else
+  #if (SPI_INTERFACES_COUNT == 1)
+    Adafruit_FlashTransport_SPI flashTransport(SS, &SPI);
+  #else
+    Adafruit_FlashTransport_SPI flashTransport(SS1, &SPI1);
+  #endif
+#endif
+
+Adafruit_SPIFlash flash(&flashTransport);
+
+// file system object from SdFat
+FatFileSystem fatfs;
+
+File myFile;
+
+void flash_setup(void) {
+  // Open serial communications and wait for port to open:
+  Serial.begin(38400);
+  while (!Serial) {
+    delay(1); // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Serial.print("\nInitializing Filesystem on external flash...");
+  
+  // Init external flash
+  flash.begin();
+
+  // Init file system on the flash
+  fatfs.begin(&flash);
+
+  Serial.println("initialization done.");
+
+  if (!fatfs.remove(FILE_NAME)) {
+    Serial.print("Failed to remove "); Serial.println(FILE_NAME);
+  }
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = fatfs.open(FILE_NAME, FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to "); Serial.print(FILE_NAME); Serial.print(" ");
+
+// file contents - - - - - - - - - - - - - - - -
+
+    myFile.print(": hello 0 do 1 drop emit loop ;\r");
+
+    myFile.print(": eamit 0 do emit loop ;\r");
+
+    myFile.print("69 68 67 66 65 5 eamit cr\r");
+
+    // myFile.print("wlist cr cr words cr\r");
+
+// file contents - - - - - - - - - - - - - - - -
+
+    // close the file:
+    myFile.close();
+    Serial.println(" has now been done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.print("error opening "); Serial.println(FILE_NAME);
+  }
+
+  // re-open the file for reading:
+
+  myFile = fatfs.open(FILE_NAME);
+  // thisFile = (File) myFile; // local tnr kludge
+
+  if (myFile) {
+    Serial.print(FILE_NAME); Serial.println(" .. will now be read and printed to the console.");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+    Serial.print(FILE_NAME); Serial.println(" .. is now closed, safely.");
+
+
+    // super kludge tnr - open it and leave it open:
+
+    // re-open the file for reading:
+
+    Serial.println("FILE STAYS OPEN");
+
+    // myFile = fatfs.open(FILE_NAME);
+
+       File dataFile = fatfs.open(FILE_NAME, FILE_READ);
+       thisFile = (File) dataFile; // local tnr kludge
+       thisFile.rewind();
+
+// 50   File dataFile = fatfs.open(FILE_NAME, FILE_READ);
+// 51   thisFile = (File) dataFile;
+
+
+
+  } else {
+    // if the file didn't open, print an error:
+    Serial.print("error opening "); Serial.println(FILE_NAME);
+  }
+}
+
+void flash_loop() {
+  // nothing happens after setup
+}
