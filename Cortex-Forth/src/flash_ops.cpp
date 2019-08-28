c2888806dafab3bea4a62709d20692c056d38a53
wa1tnr <wa1tnr@users.noreply.github.com>
2019-08-28 03:33:11 +0000

    preferred grouping works with bugfix

--- Cortex-Forth/src/flash_ops.cpp
+++ Cortex-Forth/src/flash_ops.cpp
@@ -228,11 +228,11 @@ loop 1 + swap drop cr ;
     myFile.print(": stuffit 69 68 67 66 65 5 ;\r");
     myFile.print("69 68 67 66 65 5 emits cr\r");
 
-    myFile.print("variable bend variable buff\r");
-    myFile.print("here buff ! 32 allot here bend ! 1 drop\r");
-    myFile.print(": svd buff @ 2701 + blist ; cr\r"); // so adding a 'cr' to the end of the line faked out the parser into not seeing a single character entity as the last entity on the line. ;)
-    myFile.print(": sve buff @ 4 + cr ; cr\r");
-    myFile.print(": goa svd sve 26 0 do 1 + 32 i + over ! loop cr cr svd cr ; cr\r");
+    myFile.print("variable bend variable buff here buff !\r");
+    myFile.print("32 allot here bend ! 1 drop\r");
+    myFile.print(": svd buff @ 2701 + blist ;\r"); // so adding a 'cr' to the end of the line faked out the parser into not seeing a single character entity as the last entity on the line. ;)
+    myFile.print(": sve buff @ 4 + cr ;\r");
+    myFile.print(": goa svd sve 26 0 do 1 + 32 i + over ! loop cr cr svd cr ;\r");
 
 
 
