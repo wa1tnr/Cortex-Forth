9798b448f16f1926a38e7e12db61daf67056bcd1
wa1tnr <wa1tnr@users.noreply.github.com>
2019-09-02 01:55:41 +0000

    support quiet printing mods

--- Cortex-Forth/common.h
+++ Cortex-Forth/common.h
@@ -1,1 +1,8 @@
 #define FILE_NAME      "/forth/ascii_xfer_a001.txt"
+#define WORKING_DIR "/forth"
+
+#define VERBIAGE_AA
+#undef VERBIAGE_AA
+
+#define FBUFF_PRN // echo + char during file read+print op
+#undef FBUFF_PRN
