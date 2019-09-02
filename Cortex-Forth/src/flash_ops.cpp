ee91286219097921bfd215043d4d3d5039b4e4dc
wa1tnr <wa1tnr@users.noreply.github.com>
2019-09-02 01:54:14 +0000

    extra verbiage alternately not compiled into the program

--- Cortex-Forth/src/flash_ops.cpp
+++ Cortex-Forth/src/flash_ops.cpp
@@ -29,6 +29,8 @@
 
 File thisFile;
 #include "../common.h"
+// #define WORKING_DIR "/forth"
+// #define VERBIAGE_AA #undef VERBIAGE_AA
 
 #if defined(__SAMD51__) || defined(NRF52840_XXAA)
   Adafruit_FlashTransport_QSPI flashTransport(PIN_QSPI_SCK, PIN_QSPI_CS, PIN_QSPI_IO0, PIN_QSPI_IO1, PIN_QSPI_IO2, PIN_QSPI_IO3);
@@ -47,8 +49,6 @@ FatFileSystem fatfs;
 
 File myFile;
 
-#define WORKING_DIR "/forth"
-
 void mkdir_forth(void) {
   if (!fatfs.exists(WORKING_DIR)) {
     Serial.print(WORKING_DIR);
@@ -61,12 +61,20 @@ void mkdir_forth(void) {
       delay(4000);
       while(1);
     }
+#ifdef VERBIAGE_AA
     Serial.print("Created ");
     Serial.print(WORKING_DIR);
     Serial.println(" directory!");
+#else
+    Serial.print(" +dcr ");
+#endif // #ifdef VERBIAGE_AA
   } else {
+#ifdef VERBIAGE_AA
       Serial.print(WORKING_DIR);
       Serial.println("  directory was previously created.  No worries.  Continuing.. ");
+#else
+      Serial.print(" +dpc ");
+#endif // #ifdef VERBIAGE_AA
   }
 }
 
@@ -84,6 +92,7 @@ void mkdir_forth(void) {
  83   }
 
 */
+
 void flash_setup(void) {
   // Open serial communications and wait for port to open:
   Serial.begin(38400);
@@ -91,7 +100,11 @@ void flash_setup(void) {
     delay(1); // wait for serial port to connect. Needed for native USB port only
   }
 
+#ifdef VERBIAGE_AA
   Serial.print("\nInitializing Filesystem on external flash...");
+#else
+  Serial.print(" ckpt AA ");
+#endif // #ifdef VERBIAGE_AA
   
   // Init external flash
   flash.begin();
@@ -99,7 +112,11 @@ void flash_setup(void) {
   // Init file system on the flash
   fatfs.begin(&flash);
 
+#ifdef VERBIAGE_AA
   Serial.println("initialization done.");
+#else
+  Serial.print(" ckpt BB ");
+#endif // #ifdef VERBIAGE_AA
 
   if (!fatfs.remove(FILE_NAME)) {
     Serial.print("Failed to remove "); Serial.println(FILE_NAME);
@@ -116,7 +133,11 @@ void flash_setup(void) {
 
   // if the file opened okay, write to it:
   if (myFile) {
+#ifdef VERBIAGE_AA
     Serial.print("Writing to "); Serial.print(FILE_NAME); Serial.print(" ");
+#else
+    Serial.print(" ckpt CC ");
+#endif // #ifdef VERBIAGE_AA
 
 // file contents - - - - - - - - - - - - - - - -
 
@@ -252,7 +273,11 @@ loop 1 + swap drop cr ;
 
     // close the file:
     myFile.close();
+#ifdef VERBIAGE_AA
     Serial.println(" has now been done.");
+#else
+    Serial.print(" ckpt DD ");
+#endif // #ifdef VERBIAGE_AA
   } else {
     // if the file didn't open, print an error:
     Serial.print("error opening "); Serial.println(FILE_NAME);
@@ -264,27 +289,50 @@ loop 1 + swap drop cr ;
   // thisFile = (File) myFile; // local tnr kludge
 
   if (myFile) {
+#ifdef VERBIAGE_AA
     Serial.print(FILE_NAME); Serial.println(" .. will now be read and printed");
     Serial.println("to the console.  Attention: design has strange line endings!\r\n");
+#else
+    Serial.print(" ckpt EE ");
+#endif // #ifdef VERBIAGE_AA
 
     // read from the file until there's nothing else in it:
     while (myFile.available()) {
+#ifdef VERBIAGE_AA
       Serial.write(myFile.read());
+#else
+#ifdef FBUFF_PRN // suppress when undefined
+      Serial.print("+");
+#endif
+      int ij = myFile.read(); // do the read anyway - try to break something
+#endif // #ifdef VERBIAGE_AA
     }
     // close the file:
     myFile.close();
+#ifdef VERBIAGE_AA
     Serial.println("\r\n");
     Serial.print(FILE_NAME); Serial.println(" .. is now closed, safely.");
+#else
+    Serial.print(" ckpt FF ");
+#endif // #ifdef VERBIAGE_AA
 
     // re-open the file for reading:
 
     File dataFile = fatfs.open(FILE_NAME, FILE_READ);
+#ifdef VERBIAGE_AA
     Serial.print(FILE_NAME);
     Serial.println(" is now re-opened (for reading).");
+#else
+    Serial.print(" ckpt GG ");
+#endif // #ifdef VERBIAGE_AA
 
     thisFile = (File) dataFile;
     thisFile.rewind();
+#ifdef VERBIAGE_AA
     Serial.println("FILE STAYS OPEN (and rewound) (for a possible fload).");
+#else
+    Serial.print(" ckpt HH ");
+#endif // #ifdef VERBIAGE_AA
   } else {
     // if the file didn't open, print an error:
     Serial.print("error opening "); Serial.println(FILE_NAME);
