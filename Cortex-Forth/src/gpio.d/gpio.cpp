411f88de384d87369a84518b18a99d1cd5ad8734
wa1tnr <wa1tnr@users.noreply.github.com>
2019-09-05 14:10:00 +0000

    gpio from ainsuForth source tree

--- Cortex-Forth/src/gpio.d/gpio.cpp
+++ Cortex-Forth/src/gpio.d/gpio.cpp
@@ -1,6 +1,31 @@
 #include <Arduino.h>
 
-void _PINMODE(void) { }
-void _PINWRITE(void)  {}
-void _PINREAD(void) { }
+extern void push(int n);
+extern int pop(void);
+
+// ( u1 u2 -- )
+void _PINWRITE(void)  {
+    digitalWrite(pop(), pop());
+    // digitalWrite(13, HIGH);
+}
+
+// ( u1 u2 -- )
+void _PINMODE(void) {
+    pinMode(pop(), pop());
+    // pinMode(13, OUTPUT);
+}
+
+void _PINREAD(void) {
+    push(digitalRead(pop()));
+}
+
+/*
+void _analogRead(void) {
+    // dStack_push(analogRead(dStack_pop()));
+}
+
+void _analogWrite(void) {
+    // analogWrite(dStack_pop(), dStack_pop());
+}
+*/
 
